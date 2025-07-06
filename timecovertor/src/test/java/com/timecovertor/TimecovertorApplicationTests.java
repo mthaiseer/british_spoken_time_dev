31b6cc2aa48d9f480d3502a14ff64b7d550784a0
Mohamed Thaiseer <mohamed.thaiseer@gmail.com>
2025-07-06 07:48:38 +0300

    inital commit with basic code

---
+++ timecovertor/src/test/java/com/timecovertor/TimecovertorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timecovertor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimecovertorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
